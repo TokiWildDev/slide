858b3e0c48fd0bb7dbcaf6293e6588263145f755
TokiWildDev <tokiworldwilddev@gmail.com>
2022-08-07 17:35:01 +0200

    mise en palce du javascript bouton suivant

--- javaScript/slide.js
+++ javaScript/slide.js
@@ -1,1 +1,36 @@
 console.log('helloWolrd')
+
+const items = document.getElementsByClassName('containSlide');
+const nbItems = items.length;
+const suivant = document.getElementsByClassName('slideSuivant');
+const precedent = document.getElementsByClassName('slidePrecedent');
+let count = 0 ;
+
+suivant[0].addEventListener('click',() =>
+{
+
+    items[count].classList.remove('containSlide--active');
+
+    if(count < nbItems - 1)
+    {
+        count++;
+        console.log(count);
+    }
+    else
+    {
+        count=0;
+        console.log(count);   
+    }
+     
+    items[count].classList.add('containSlide--active');
+
+});
+
+
+precedent[0].addEventListener('click',() =>
+{
+    console.log('précedent')
+});
+
+
+
